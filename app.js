4cadf79478788aff442b1abfd2e9a9965aa867c8
Danboxx <danisloglapro@gmail.com>
2024-12-05 17:21:52 +0100

    MusicHalle FDJ

--- app.js
+++ app.js
@@ -10,18 +10,22 @@ fetch("data.json")
     //je boucle sur le tableau et pour chacun des produits : je l'affiche
     donnee.produits.forEach(produits => {
         afficheUnProduit(produits)
-    
-    donnee.services.forEach(services)
-        afficheUnService(services)
     });
+    donnee.services.forEach(service => {
+        afficheUnService(service)
+    });
+    donnee.temoignages.forEach(temoignage => {
+        afficheUnTemoignage(temoignage)
+    });
+    
 })
 function afficheInfos(info){
     document.querySelector("#nomEntreprise").innerHTML = info.nomEntreprise
 }
 
 function afficheUnProduit(p){
-    // ROle: affiche un produit dans la div qui a l'id "produits"
-    //parametre : p, le produit à afficher
+    // Role: affiche un produit dans la div qui a l'id produits
+    // parametre : p, le produit à afficher
     // retour rien
     console.log(p.nom)
     console.log(p.description)
@@ -35,8 +39,8 @@ function afficheUnProduit(p){
 }
 
 function afficheUnService(p){
-    // ROle: affiche un service dans la div qui a l'id "services"
-    //parametre : p, le service à afficher
+    // Role: affiche un service dans la div qui a l'id "services"
+    // parametre : p, le service à afficher
     // retour rien
     console.log(p.nom)
     console.log(p.description)
@@ -46,5 +50,26 @@ function afficheUnService(p){
             <h4>${p.nom}</h4>
             <p>${p.description}</p>
         </div>`
-    document.querySelector("#services)").innerHTML += template
+    document.querySelector("#services").innerHTML += template
+}
+
+function afficheUnTemoignage(p){
+    // Role: affiche un temoignage dans la div qui a l'id "temoignage"
+    // parametre : p, le temoignage à afficher
+    // retour rien
+    console.log(p.prenom)
+    console.log(p.prestation)
+    console.log(p.commentaire)
+    console.log(p.note)
+    let template = `<div class="card>"
+            <h4>${p.prenom}</h4>
+            <p>${p.prestation}</p>
+            <p>${p.commentaire}</p>
+            <p>${etoile(p.note)}</p>
+        </div>`
+    document.querySelector("#temoignages").innerHTML += template
+}
+
+function etoile(note) {
+    return "★".repeat(note) + "☆".repeat(5-note)
 }
